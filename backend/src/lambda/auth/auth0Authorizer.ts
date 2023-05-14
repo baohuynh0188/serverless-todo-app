5cab27a82c3d90e223cc8e264e69b8c84e03320b
Bao Huynh <scorers_punk0d@icloud.com>
2023-05-14 14:16:32 +0700

    store secretKey

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,4 +1,5 @@
 import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda';
 import { verify } from 'jsonwebtoken';
 import { createLogger } from '../../utils/logger';
@@ -11,7 +12,12 @@ const logger = createLogger('auth');
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const auth0Secret = process.env.AUTH_0_SECRET;
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+let cachedSecret: string = '';
 
 export const handler = async (
     event: CustomAuthorizerEvent
@@ -60,7 +66,11 @@ const verifyToken = async (authHeader: string): Promise<JwtPayload> => {
     // TODO: Implement token verification
     // You should implement it similarly to how it was implemented for the exercise for the lesson 5
     // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-    return verify(token, auth0Secret) as JwtPayload;
+
+    const secretObject: any = await getSecret();
+    const secret = secretObject[secretField];
+
+    return verify(token, secret) as JwtPayload;
 };
 
 const getToken = (authHeader: string): string => {
@@ -73,4 +83,14 @@ const getToken = (authHeader: string): string => {
     const token = split[1];
 
     return token;
+};
+
+const getSecret = async () => {
+    if (cachedSecret) return cachedSecret;
+
+    const data = await client.getSecretValue({ SecretId: secretId }).promise();
+
+    cachedSecret = data.SecretString;
+
+    return JSON.parse(cachedSecret);
 };
